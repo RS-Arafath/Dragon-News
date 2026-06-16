c0af6a1fe4cb9569b63b3cd4a340a82bb1112666
Arafath <rsarafath.signin@gmail.com>
2026-06-16 23:36:34 +0600

    Refactor getCategories function for async handling and update Home component to await its result; enhance category display structure

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,18 +1,52 @@
-async function GetCategories() {
+async function getCategories() {
   const res = await fetch(
-    'https://openapi.programming-hero.com/api/news/categories'
+    'https://openapi.programming-hero.com/api/news/categories',
   );
-  const data = res.json();
-  return data
-  
+  const data = await res.json();
+  return data.data.news_category;
 }
 
 export default async function Home() {
-  const categories = GetCategories()
-  console.log(categories);
+  const categories = await getCategories();
+  console.log('categories data', categories);
   return (
-    <div className="grid grid-cols-4 gap-5 h-100">
-      <div className="bg-red-200 ">category</div>
+    <div className="grid grid-cols-4 gap-5 ">
+      <div className="bg-gray-100 rounded p-5">
+        <h2 className="text-xl sm:text-2xl text-black font-bold text-center bg-slate-200 p-3 rounded">
+          All Categories
+        </h2>
+        <ul className="flex flex-col my-5 gap-3">
+          {categories.map((category) => {
+            return (
+              <li
+                key={category.category_id}
+                className="
+        group
+        border border-gray-200
+        p-2 sm:p-3
+        rounded-2xl
+        text-lg sm:text-xl
+        font-semibold
+        text-center
+        cursor-pointer
+        transition-all duration-200
+
+        hover:-translate-y-1
+      
+        hover:border-red-600
+        hover:shadow-lg
+        hover:shadow-red-500/20
+        hover:text-red-600
+        active:scale-90
+active:translate-y-0
+      "
+              >
+                {category.category_name}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
       <div className="bg-pink-200 col-span-2 ">main</div>
       <div className="bg-blue-200 ">social</div>
     </div>
