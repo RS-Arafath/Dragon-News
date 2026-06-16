353dc6308348da504d778bb79a132f54767da58a
Arafath <rsarafath.signin@gmail.com>
2026-06-16 12:38:56 +0600

    Enhance NavLink component to highlight active link; clean up Navbar imports

--- src/components/shared/NavLink.jsx
+++ src/components/shared/NavLink.jsx
@@ -5,8 +5,9 @@ import React from 'react';
 
 const NavLink = ({ href, children }) => {
   const pathName = usePathname()
+  const isActive=href===pathName
   console.log(pathName);
-  return <Link href={href}>{children}</Link>
+  return <Link href={href} className={`${isActive? 'border-b-2 border-red-500 font-semibold text-red-600 ':""}`}>{children}</Link>
 };
 
 export default NavLink;
