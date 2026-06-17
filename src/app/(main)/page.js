91d68eee3a820418ceea35f30980c95ca421e75e
Arafath <rsarafath.signin@gmail.com>
2026-06-17 23:42:42 +0600

    Fix API endpoint for fetching news by category ID and update news display in Home component

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -11,20 +11,27 @@ async function getCategories() {
 }
 async function getNewsByCategoryId(category_id) {
   const res = await fetch(
-    `https://openapi.programming-hero.com/api/news/categories/${category_id}`,
+    `https://openapi.programming-hero.com/api/news/category/${category_id}`,
   );
   const data = await res.json();
-  return data.data.news_category;
+  return data.data
 }
 
 export default async function Home() {
   const categories = await getCategories();
   //console.log('categories data', categories);
+  const news = await getNewsByCategoryId("04");
+  console.log('news',news);
   return (
     <div className="grid grid-cols-4 gap-5 ">
       <CategoryBox categories={categories} activeId={'01'}></CategoryBox>
 
-      <div className="bg-pink-200 col-span-2 ">main</div>
+      <div className="bg-pink-200 col-span-2 ">{
+        news.map(n => {
+          return <div key={n._id}>{n.title }</div>
+      })
+      
+      }</div>
       <div className="">
         <RightSideBar></RightSideBar>
       </div>
