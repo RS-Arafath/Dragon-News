58c2896c655379c02ed9a80b2bdfb1ed581196a5
Arafath <rsarafath.signin@gmail.com>
2026-06-17 23:29:20 +0600

    Add fetch function for news by category ID and improve code organization

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,9 +1,17 @@
 import CategoryBox from '@/components/homepage/news/CategoryBox';
 import RightSideBar from '@/components/homepage/news/RightSideBar';
 
+/* right side category fetch */
 async function getCategories() {
   const res = await fetch(
     'https://openapi.programming-hero.com/api/news/categories',
+  );
+  const data = await res.json();
+  return data.data.news_category;
+}
+async function getNewsByCategoryId(category_id) {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/news/categories/${category_id}`,
   );
   const data = await res.json();
   return data.data.news_category;
@@ -15,7 +23,6 @@ export default async function Home() {
   return (
     <div className="grid grid-cols-4 gap-5 ">
       <CategoryBox categories={categories} activeId={'01'}></CategoryBox>
-
 
       <div className="bg-pink-200 col-span-2 ">main</div>
       <div className="">
