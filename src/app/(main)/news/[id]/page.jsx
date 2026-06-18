bf4f6df3190abcd5476b34c3b10f690dcb4cc416
Arafath <rsarafath.signin@gmail.com>
2026-06-18 23:47:44 +0600

    Fix console log to correctly display news ID from params

--- src/app/(main)/news/[id]/page.jsx
+++ src/app/(main)/news/[id]/page.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-const newsPage = ({params}) => {
+const newsPage =async ({ params }) => {
+  const {id} = await params;
+  console.log(id,'params res');
   return (
     <div>
       news
